61e1d4cb7a5f129af52762cf7ac02f16abcfe90e
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-07-29 15:19:59 +0100

    Update script.js to create board

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log(location.pathname);
+
+// html elements for auto-complete
+board;
+
+// create board
+for (let j = 0; j < 8; j++) {
+    const rank = document.createElement("div");
+    for (let i = 0; i < 8; i++) {
+        const square = document.createElement("div");
+        square.style.height = square.style.width = "100px";
+        square.style.backgroundColor = (j % 2 === i % 2) ? "white": "black";
+        square.style.display = "inline-block";
+        rank.appendChild(square);
+    }
+    board.appendChild(rank);
+}
