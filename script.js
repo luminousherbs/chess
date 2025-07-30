f0a2fe2a4f3eadaab6e8c2cd10695af2002388f9
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-07-30 11:37:30 +0100

    Give squares coordinates

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 console.log(location.pathname);
 
+// functions
+function squareAt(x, y) {
+    return document.querySelectorAll(`[data-x="${x}"][data-y="${y}"]`)
+}
+
 // html elements for auto-complete
 board;
 
 // create board
-for (let j = 0; j < 8; j++) {
+for (let y = 0; y < 8; y++) {
     const rank = document.createElement("div");
-    for (let i = 0; i < 8; i++) {
+    for (let x = 0; x < 8; x++) {
         const square = document.createElement("div");
         square.style.height = square.style.width = "100px";
-        square.style.backgroundColor = (j % 2 === i % 2) ? "white": "black";
+        square.style.backgroundColor = (y % 2 === x % 2) ? "white": "black";
         square.style.display = "inline-block";
+        square.dataset.y = y;
+        square.dataset.x = x;
         rank.appendChild(square);
     }
     board.appendChild(rank);
 }
+console.log(squareAt(4, 7));
