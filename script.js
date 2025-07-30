6b9702a83c93ecd4a45a5816394051bb794544fa
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-07-30 11:51:08 +0100

    Move styling to separate file

--- script.js
+++ script.js
@@ -13,9 +13,8 @@ for (let y = 0; y < 8; y++) {
     const rank = document.createElement("div");
     for (let x = 0; x < 8; x++) {
         const square = document.createElement("div");
-        square.style.height = square.style.width = "100px";
         square.style.backgroundColor = (y % 2 === x % 2) ? "white": "black";
-        square.style.display = "inline-block";
+        square.className = "square";
         square.dataset.y = y;
         square.dataset.x = x;
         rank.appendChild(square);
